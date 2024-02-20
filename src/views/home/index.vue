5df6215287a6b35ed2c8c329e7a12862892f886d
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-20 19:02:55 +0800

    add panel for good description

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,12 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+
+<HomePanel title="Fresh" subTitle="Fresh good"> 
+<div>woshi slot</div>
+</HomePanel>
+<HomePanel title="Recommend"> 
+
+</HomePanel>
+
 </template>
