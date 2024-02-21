22ffb8ee5900eece700693db4f0cb99ff69c3e1d
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-21 14:33:33 +0800

    build goodItem component

--- src/views/home/components/HomeProduct.vue
+++ src/views/home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import {getGoodsAPI} from '@/apis/home'
 import {ref, onMounted} from 'vue'
+import GoodsItem from './GoodsItem.vue';
 
 
 const goodsProduct = ref([])
@@ -30,12 +31,7 @@ onMounted(() => {
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :goods="good" />
           </li>
         </ul>
       </div>
