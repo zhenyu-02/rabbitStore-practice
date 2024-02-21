93f72d590643f8cdf0f32b34b487b30305fdb0e1
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-21 15:49:38 +0800

    add banner in Category

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -2,6 +2,8 @@
 import {getCategoryAPI} from '@/apis/category'
 import {ref, onMounted} from 'vue'
 import {useRoute} from 'vue-router'
+import {getBannerAPI} from '@/apis/home'
+
 
 const categoryData = ref({})
 const route = useRoute()
@@ -10,8 +12,20 @@ const getCategory = async () => {
   categoryData.value = res.result
   console.log(categoryData.value.name);
 }
+//watch the value of id Instead of onMounted
 onMounted(() => getCategory())
 
+const bannerList = ref([])
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite:'2'
+  })
+  console.log(res.result);
+  bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
 
 
 </script>
@@ -26,6 +40,15 @@ onMounted(() => getCategory())
           <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -107,6 +130,21 @@ onMounted(() => getCategory())
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  // position: absolute;
+  margin:0 auto;
+  left: 0;
+  top: 0;
+  // z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
