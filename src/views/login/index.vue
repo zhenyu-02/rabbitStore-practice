a572a4e855473dee51b436cc37b6ed3872279d39
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-24 15:19:23 +0800

    check all input when submit

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -29,6 +29,16 @@ const rules = {
   ]
 }
 
+const formRef = ref(null)
+const doLogin = () => {
+  formRef.value.validate((valid)=> {
+    // console.log(valid);
+    if (valid) {
+      // login
+    }
+  })
+}
+
 </script>
 
 
@@ -53,7 +63,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account"/>
@@ -66,7 +76,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
