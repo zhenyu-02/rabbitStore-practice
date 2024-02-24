85638f13ee1b30df9bdbe18fc7f7f7e70ae7e27d
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-24 13:27:44 +0800

    add focuse for ImageView in Detail page

--- src/components/imageView/index.vue
+++ src/components/imageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from 'vue'
+import {useMouseInElement, watchArray} from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -15,16 +16,46 @@ const enterhandler = (i) => {
   activeIndex.value = i  
 }
 
+const target = ref(null)
+const {elementX, elementY, isOutside} = useMouseInElement(target)
+
+const left =ref(0)
+const top = ref(0)
+watchArray([elementX, elementY], () => {
+  console.log(('Mouse is Moving'));
+  if (elementX.value > 100 && elementX.value<300) {
+    left.value = elementX.value-100
+  }
+  if (elementY.value > 100 && elementY.value<300) {
+    top.value = elementY.value-100
+  }
+
+  if (elementX.value>300) {
+    left.value = 200
+  }
+  if (elementX.value<100){
+    left.value=0
+  }
+  if (elementY.value>300) {
+    top.value = 200
+  }
+  if (elementY.value<100){
+    top.value=0
+  }
+
+})
+
 </script>
 
 
 <template>
+  <!-- {{ {elementX, elementY, isOutside} }} -->
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
