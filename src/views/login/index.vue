9c885112b1d57cc5ad8d5c1d865040b1828da1da
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-24 15:12:47 +0800

    add check for agreement Loagin Page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,8 @@ import {ref} from 'vue'
 
 const form = ref({
   account:'',
-  password:''
+  password:'',
+  agree:true
 })
 
 const rules = {
@@ -13,6 +14,18 @@ const rules = {
   password:[
     {required:true, message:'密码不能为空',trigger:'blur'},
     {min:6, max:14, message:'密码必须为6-14位',trigger:'blur'}
+  ],
+  agree:[
+    {
+      validator:(rule,value, callback) => {
+        // console.log(value);
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
+      }
+    }
   ]
 }
 
@@ -48,8 +61,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
