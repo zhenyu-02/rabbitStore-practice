d7e1261078b1d15756af1e85b97e5cc312c84841
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-24 08:37:27 +0800

    配置二级路由框架

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Login from '@/views/login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 
 const router = createRouter({
@@ -20,6 +21,10 @@ const router = createRouter({
         {
           path:'category/:id',
           component:Category
+        },
+        {
+          path:'category/sub/:id',
+          component:SubCategory
         }
       ]
     },
