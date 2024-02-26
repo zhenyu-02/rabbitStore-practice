538fb64c82642841238adc019b15256ac4f4ad07
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-26 12:01:48 +0800

    generate orderid and rende Cart in Checkout Page

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,7 +1,11 @@
 <script setup>
-import {getCheckInfoAPI} from '@/apis/checkout'
+import {getCheckInfoAPI, createOrderAPI} from '@/apis/checkout'
 import {ref, onMounted} from 'vue'
+import { useRouter } from 'vue-router';
+import { useCartStore } from '@/stores/cartStore';
 
+const cartStore = useCartStore()
+const router = useRouter()
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})
 const getCheckInfo = async() => {
@@ -24,6 +28,31 @@ const confirm = () => {
   curAddress.value = activeAddress.value
   showDialog.value = false
   activeAddress.value = {}
+}
+
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: '',
+    goods: checkInfo.value.goods.map(item => {
+      return {
+        skuId: item.skuId,
+        count: item.count
+      }
+    }),
+    addressId: curAddress.value.id
+  })
+
+  const orderId = res.result.id
+  router.push({
+    path:'pay',
+    query:{
+      id:orderId
+    }
+  })
+  cartStore.updataNewList()
 }
 
 </script>
@@ -120,7 +149,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large" >提交订单</el-button>
         </div>
       </div>
     </div>
