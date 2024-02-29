e0aef5458ed663ed0b3c098effc92d1dcd6bc0a4
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-29 14:58:56 +0800

    add Member part in Router

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/Userinfo.vue'
+import UserOrder from '@/views/Member/components/Userorder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +47,19 @@ const router = createRouter({
         {
           path:'pay',
           component:Pay
+        },{
+          path:'member',
+          component:Member,
+          children:[
+            {
+              path:'user',
+              component:UserInfo
+            },
+            {
+              path:'order',
+              component:UserOrder
+            }
+          ]
         }
       ]
     },
