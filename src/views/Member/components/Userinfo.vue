9a925ec4034abce4a7ca77a7e6c1e64fae69db60
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-02-29 15:25:23 +0800

    add orders in Member

--- src/views/Member/components/Userinfo.vue
+++ src/views/Member/components/Userinfo.vue
@@ -1,5 +1,20 @@
 <script setup>
-const userStore = {}
+import {getLikeListAPI} from '@/apis/user'
+import {useUserStore} from '@/stores/userStore'
+import {ref, onMounted} from 'vue'
+import GoodsItem from '@/views/home/components/GoodsItem.vue'
+
+const userStore = useUserStore()
+
+const likeList = ref([])
+
+const getLikeList = async () => {
+  const res = await getLikeListAPI({limit:4})
+  likeList.value = res.result
+}
+
+onMounted(() => getLikeList())
+
 </script>
 
 <template>
@@ -32,7 +47,7 @@ const userStore = {}
         <h4 data-v-bcb266e0="">猜你喜欢</h4>
       </div>
       <div class="goods-list">
-        <!-- <GoodsItem v-for="good in likeList" :key="good.id" :good="good" /> -->
+        <GoodsItem v-for="good in likeList" :key="good.id" :good="good" />
       </div>
     </div>
   </div>
